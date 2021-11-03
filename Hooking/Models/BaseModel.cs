e109bb132eb5882e909a21673eca2624cda99b4e
Pedja <despotovicpedja@gmail.com>
2021-11-03 20:15:09 +0100

    Adding project

---
+++ Hooking/Models/BaseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hooking.Models
+{
+    public class BaseModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
