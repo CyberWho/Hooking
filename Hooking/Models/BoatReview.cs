dbe755bc8b5978423698504055f8ce4bdf5f2abe
aleksandramitro <sandramitro99@gmail.com>
2021-11-18 14:57:52 +0100

    added display names to all boat classes

--- Hooking/Models/BoatReview.cs
+++ Hooking/Models/BoatReview.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Hooking.Models
 {
@@ -9,9 +10,13 @@ namespace Hooking.Models
     {
         public string BoatId { get; set; }
         public string UserDetailsId { get; set; }
+        [DisplayName("Datum objave")]
         public DateTime CreationTime { get; } = DateTime.Now;
+        [DisplayName("Recenzija")]
         public string Review { get; set; } // limited to 300 chars
+        [DisplayName("Ocena")]
         public string Grade { get; set; }
+        [DisplayName("Da li je odobrena recenzija?")]
         public bool IsApproved { get; set; }
     }
 }
--- Hooking/Models/CancelationPolicy.cs
+++ Hooking/Models/CancelationPolicy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,9 @@ namespace Hooking.Models
 {
     public class CancelationPolicy : BaseModel
     {
+        [DisplayName("Besplatno otkazivanje do")]
         public int FreeUntil { get; set; }
+        [DisplayName("Procenat od cene koji se čuva u slučaju otkaza")]
         public int PenaltyPercentage { get; set; }
     }
 }
