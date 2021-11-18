d7600d9f5bafbfb86f5d9ffee3a014099bcb0274
aleksandramitro <sandramitro99@gmail.com>
2021-11-18 14:46:50 +0100

    added displayeName for each cottage class

--- Hooking/Models/CottageRoom.cs
+++ Hooking/Models/CottageRoom.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,13 @@ namespace Hooking.Models
 {
     public class CottageRoom : BaseModel
     {
+        [DisplayName("Broj kreveta")]
         public int BedCount { get; set; }
+        [DisplayName("Klima uređaj")]
         public bool AirCondition { get; set; }
+        [DisplayName("TV")]
         public bool TV { get; set; }
+        [DisplayName("Terasa")]
         public bool Balcony { get; set; }
     }
 }
