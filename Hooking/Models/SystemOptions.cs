80d357e8cd1a2cb0b0411edffc5d8f11ebc70537
CyberWho <despot@uns.ac.rs>
2021-12-07 21:17:34 +0100

    system-options

--- Hooking/Models/SystemOptions.cs
+++ Hooking/Models/SystemOptions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace Hooking.Models
 {
     public class SystemOptions : BaseModel
     {
+        [Display(Name="Podešavanje")]
         public string OptionName { get; set; }
+
+        [Display(Name = "Vrednost")]
         public string OptionValue { get; set; }
     }
 }
