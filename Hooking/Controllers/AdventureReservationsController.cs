669d83e0ad455672653fa8dbb7116a23d2a22ede
aleksandramitro <sandramitro99@gmail.com>
2021-12-10 20:57:49 +0100

    fix-after-merge

--- Hooking/Controllers/AdventureReservationsController.cs
+++ Hooking/Controllers/AdventureReservationsController.cs
@@ -83,6 +83,40 @@ namespace Hooking.Controllers
 
             return View(adventureReservation);
         }
+        [HttpGet]
+        public IActionResult CreateView(Guid id, Guid cId)
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateView(Guid id, Guid cId, [Bind("Id,RowVersion")] AdventureReservation adventureReservation)
+        {
+            if (ModelState.IsValid)
+            {
+                adventureReservation.Id = Guid.NewGuid();
+                adventureReservation.AdventureRealisationId = cId.ToString();
+                adventureReservation.UserDetailsId = id.ToString();
+                adventureReservation.IsReviewed = false;
+                await _context.SaveChangesAsync();
+
+                return Redirect("/Adventures/Index");
+            }
+            return Redirect("/Adventures/Index");
+
+        }
+        public async Task<IActionResult> AdventureReservationHistory()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            UserDetails userDetails = _context.UserDetails.FirstOrDefault(u => u.IdentityUserId == userId);
+
+            if (userDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(_adventureService.GetAdventureReservationsHistory(userDetails.Id));
+        }
 
         // POST: AdventureReservations/Delete/5
         [HttpPost, ActionName("Delete")]
