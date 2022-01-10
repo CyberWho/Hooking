364e4f2efc65c215b1b5856e3041daf870d524f6
CyberWho <despot@uns.ac.rs>
2022-01-10 19:11:11 +0100

    adventure-appeals

--- Hooking/Models/AdventureAppeal.cs
+++ Hooking/Models/AdventureAppeal.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace Hooking.Models
 {
-    public class AdventureAppeal:BaseModel
+    public class AdventureAppeal : BaseModel
     {
         public string AdventureId { get; set; }
         [DisplayName("Datum objave")]
         public DateTime CreationTime { get; } = DateTime.Now;
-        [DisplayName("Zalba")]
+        [DisplayName("Žalba")]
         public string AppealContent { get; set; } // limited to 300 chars
+        [DisplayName("Email korisnika")]
+        public string UserEmail { get; set; }
     }
 }
